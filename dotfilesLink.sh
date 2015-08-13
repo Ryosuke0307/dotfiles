80a098196dc7c89ba00264c40c74a2e85fe22c00
川村亮輔 <ryosuke@kawamuraryousuke-no-MacBook-Air.local>
2015-08-13 15:10:57 +0900

    branch名表示など

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,6 +4,7 @@ ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/ftdetect ~/.vim
 ln -sf ~/dotfiles/indent ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
